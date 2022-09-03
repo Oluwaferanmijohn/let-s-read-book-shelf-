d71865ccf1a8c5c39594e12a03785973bc7d3651
oluwatoyin oluwaferanmi john <oluwatoyinferanmijohn>
2022-09-03 02:55:51 +0100

    new changes

--- server/register.js
+++ server/register.js
@@ -21,21 +21,94 @@ registerNow.addEventListener('click', () => {
     register();
 });
 
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+let fullname = document.getElementById('fullname');
+let username = document.getElementById('username');
+
 async function register() {
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-    let fullname = document.getElementById('fullname').value;
-    let username = document.getElementById('username').value;
+    email = email.value;
+    password = password.value;
+    fullname = fullname.value;
+    username = username.value;
 
     try {
+
         const data = await createUserWithEmailAndPassword(auth, email, password);
         let userId = data.user.uid
         await setDoc(doc(db, 'users', userId), { name: fullname, username: username, email: email })
+
         alert("Account Created Successfully")
-        window.location.replace("../dashboard/dashboard.html")
+        window.location.replace("../login/login.html")
     } catch (err) {
         if (err.message == "Firebase: Error (auth/email-already-in-use).") {
             alert("Email is taken")
         }
     }
 }
+
+
+
+
+username.addEventListener('focus', function() {
+    errorMessage.textContent = '';
+})
+username.addEventListener('blur', function() {
+    if (username.value.length < 5) {
+        errorMessage.textContent = '❗Enter a valid username';
+        errorMessage.style.color = 'red'
+        errorMessage.style.fontSize = '0.7em'
+
+    }
+    setTimeout(function() {
+        errorMessage.style.display = 'none';
+
+    }, 3000)
+})
+
+username.addEventListener('keyup', function() {
+    if (username.value.length >= 5) {
+        errorMessage.textContent = '✔valid username';
+        errorMessage.style.color = 'green'
+    } else {
+        errorMessage.textContent = '❌invalid username';
+        errorMessage.style.color = 'red'
+    }
+    setTimeout(function() {
+        errorMessage.style.display = 'none';
+
+    }, 3000)
+})
+email.addEventListener('keyup', function() {
+    let emailMessage = document.getElementById('email-message');
+    let emailMatch = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (emailMatch.test(email.value)) {
+        emailMessage.textContent = '✔Valid email';
+        emailMessage.style.color = 'green';
+    } else {
+        emailMessage.textContent = ' ❌Invalid email';
+        emailMessage.style.color = 'red';
+    }
+    setTimeout(function() {
+        emailMessage.style.display = 'none';
+
+    }, 5000)
+})
+
+// confirmPassword.addEventListener('keyup', function() {
+//     let message = document.getElementById('message');
+//     let confirmPassword = document.getElementById('confirmPassword').value;
+//     password = document.getElementById('password').value;
+//     if (password != confirmPassword) {
+//         message.textContent = ' ❌Invalid email';
+//         message.style.color = 'red';
+//     } else {
+//         message.textContent = '✔password match';
+//         message.style.color = 'green'
+//     }
+
+//     setTimeout(function() {
+//         message.style.display = 'none';
+
+//     }, 3000)
+// })
